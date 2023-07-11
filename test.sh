51631d672dc691cdfe6bbce37ef73c446d8ed81a
RAJESHKUMARSAMUDU <69385264+RAJESHKUMARSAMUDU@users.noreply.github.com>
2023-07-11 07:01:42 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -133,7 +133,63 @@ done
 
 
 
+
+
     json_object=$(echo "[{'id': '"$PROJECT_ID"', 'name': '"$PROJECT_NAME"', 'string_exists': 'yes'}]" | jq -c .)
 
     # Append the JSON object to the array
     JSON_ARRAY=$(echo "$JSON_ARRAY" | jq ". + [$json_object]")
+
+
+
+
+    #!/bin/bash
+
+# Set your GitLab token, group ID and search string
+TOKEN="YOUR_GITLAB_TOKEN"
+GROUP_ID="YOUR_GROUP_ID"
+SEARCH_STRING="YOUR_SEARCH_STRING"
+
+# Create a CSV file and add the header
+echo "id,name,string_exists" > output.csv
+
+# Initialize the page number
+PAGE=1
+
+# Loop over each page
+while true; do
+  # Get the projects on the current page
+  PROJECTS=$(curl --header "PRIVATE-TOKEN: $TOKEN" "https://gitlab.com/api/v4/groups/$GROUP_ID/projects?page=$PAGE&per_page=100" | jq -r '.[] | {id: .id, name: .name}')
+
+  # Break the loop if there are no more projects
+  if [ -z "$PROJECTS" ]; then
+    break
+  fi
+
+  # Loop over each project
+  for row in $(echo "${PROJECTS}" | jq -r '. | @base64'); do
+    # Get the project details
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+    PROJECT_ID=$(_jq '.id')
+    PROJECT_NAME=$(_jq '.name')
+
+    # Get the .gitlab-ci.yml file
+    GITLAB_CI=$(curl --header "PRIVATE-TOKEN: $TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/repository/files/%2Egitlab-ci%2Eyml?ref=master" | jq -r '.content' | base64 -d)
+
+    # Search for the string in the .gitlab-ci.yml file
+    if echo $GITLAB_CI | grep -q $SEARCH_STRING; then
+      # Add the project to the CSV file with "yes"
+      echo "$PROJECT_ID,$PROJECT_NAME,yes" >> output.csv
+    else
+      # Add the project to the CSV file with "no"
+      echo "$PROJECT_ID,$PROJECT_NAME,no" >> output.csv
+    fi
+  done
+
+  # Increment the page number
+  PAGE=$((PAGE + 1))
+done
+

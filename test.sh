003f14bc55379f806877bca59306d8600a262054
RAJESHKUMARSAMUDU <69385264+RAJESHKUMARSAMUDU@users.noreply.github.com>
2023-07-11 08:35:37 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -193,3 +193,67 @@ while true; do
   PAGE=$((PAGE + 1))
 done
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+usage() {
+if [ $# -ne 4 ]
+then
+        echo "ERROR: Incorrect usage "
+        echo "Usage :- $0 gitlab-private-token gitlab-group-id project-id-file-name search-string"
+        exit 1
+fi
+}
+
+usage $*
+
+TOKEN=$1
+GROUP_ID=$2
+FILE_NAME=$3
+SEARCH_STRING=$4
+
+total_pages=`curl --head -H "PRIVATE-TOKEN:$TOKEN" "https://gitlab.com/api/v4/groups/$GROUP_ID/projects?per_page=100" | grep -i "X-Total-Pages" | sed 's|X-Total-Pages: ||g' | tr -d '\r'`
+rm $FILE_NAME
+for (( ctr=1; ctr<=$total_pages; ctr++ ))
+do
+   echo 'Retrieving the details for the group - page '$ctr
+   curl -H "PRIVATE-TOKEN:$TOKEN" "https://gitlab.com/api/v4/groups/$GROUP_ID/projects?per_page=100&page="$ctr | jq '.[] | [.id,.name,.last_activity_at,.archived] | @csv' >> $FILE_NAME
+done
+
+# Create a CSV file and add the header
+echo "id,name,string_exists" > output.csv
+
+# Read the project IDs from the CSV file
+PROJECT_IDS=$(cut -d',' -f1 $FILE_NAME)
+
+# Loop over each project ID
+for PROJECT_ID in $PROJECT_IDS; do
+  # Get the project name
+  PROJECT_NAME=$(grep $PROJECT_ID $FILE_NAME | cut -d',' -f2)
+
+  # Get the .gitlab-ci.yml file
+  GITLAB_CI=$(curl --header "PRIVATE-TOKEN: $TOKEN" "https://gitlab.com/api/v4/projects/$PROJECT_ID/repository/files/%2Egitlab-ci%2Eyml?ref=master" | jq -r '.content' | base64 -d)
+
+  # Search for the string in the .gitlab-ci.yml file
+  if echo $GITLAB_CI | grep -q $SEARCH_STRING; then
+    # Add the project to the CSV file with "yes"
+    echo "$PROJECT_ID,$PROJECT_NAME,yes" >> output.csv
+  else
+    # Add the project to the CSV file with "no"
+    echo "$PROJECT_ID,$PROJECT_NAME,no" >> output.csv
+  fi
+done
